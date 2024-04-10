78856c82e21668e011d8368cb56080c03f1ba723
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-04-10 13:20:26 -0400

    Added nav text

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,8 +3,12 @@ import logotext from "../logotext.svg";
 
 function Navbar(){
   return(
-    <nav className="h-16 pt-2 pb-2 pl-4">
+    <nav className="h-16 pt-2 pb-2 pl-4 pr-4 flex justify-between">
       <img src={logotext} className="h-14" alt="logo" />
+      <ul className="flex items-center">
+        <li className="pr-2 inline">Trips</li>
+        <li className="pl-2 inline">Plan a Trip</li>
+      </ul>
     </nav>
   )
 }
