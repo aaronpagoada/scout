bda8dfe7aae01689f1f8fcd1ba7a9ec1ae9af540
Aaron Pagoada <74792335+aaronpagoada@users.noreply.github.com>
2024-09-26 00:54:54 -0400

    Added type to b64 func

--- frontend/src/js/getB64FromBuffer.ts
+++ frontend/src/js/getB64FromBuffer.ts
@@ -1,6 +1,6 @@
 import { Buffer } from "buffer";
 
-function getB64FromBuffer(buffer: Buffer) {
+function getB64FromBuffer(buffer: Buffer): string {
   return Buffer.from(buffer).toString("base64")
 }
 
