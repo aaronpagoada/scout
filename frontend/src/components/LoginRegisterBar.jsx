500922ef30a19a1c975feb7a0e1eca08e485ad8a
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-10-02 23:39:02 -0400

    Updated login bar

--- frontend/src/components/LoginRegisterBar.jsx
+++ frontend/src/components/LoginRegisterBar.jsx
@@ -4,23 +4,25 @@ import logotext from "../logotext.svg";
 
 function LoginRegisterBar(){
   return(
-    <nav className="h-16 pt-2 pb-2 pl-8 pr-8 flex justify-between">
-      <Link to="/">
-        <img src={logotext} className="h-14" alt="logo" />
-      </Link>
-      <ul className="flex items-center">
-        <li className="font-crimson text-xl pr-3 inline">
-          <Link to="/login">
-            Log in
-          </Link>
-        </li>
-        <li className="font-crimson text-xl pl-3 inline">
-          <Link to="/register">
-            Register
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="w-screen fixed top-0 z-40 drop-shadow">
+      <nav className="bg-yellow-100 h-20 pt-2 pb-2 pl-8 pr-8 flex justify-between">
+        <Link className="flex items-center" to="/">
+          <img src={logotext} className="h-14" alt="logo" />
+        </Link>
+        <ul className="flex items-center">
+          <li className="font-crimson text-xl pr-3 inline">
+            <Link to="/login">
+              Log in
+            </Link>
+          </li>
+          <li className="font-crimson text-xl pl-3 inline">
+            <Link to="/register">
+              Register
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
