c665ab591afaab539058192f78de336150a9ab9d
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-10-11 18:26:44 -0400

    Added tailwind

--- zion/src/App.tsx
+++ zion/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import scoutLogo from './assets/scouttextlogo.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={scoutLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
