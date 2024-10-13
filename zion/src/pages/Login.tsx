84a97e31a4aa2535c4d37a4fd795cb26bdc6a49d
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-10-13 06:00:43 -0400

    Added login and register + salome ramble

---
+++ zion/src/pages/Login.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+
+function LoginPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+}
+
+export default LoginPage
+// kilimanjaro mountain .comnm site scout jgfdfhuige
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+
+    //   ododscbked
+    //   dfkas'd9-cxx  
+
+    //   dicjx  
+    //   l---=-=<u><object data="1
+    //   @EWPed 
+      
+    //   \]0" type=""></object></u>
+    // ]
---
+++ zion/src/pages/Register.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function RegisterPage() {
+  const [name, setName] = useState("")
+
+  return (
+    <>
+      <h1>Register</h1>
+    </>
+  )
+}
+
+export default RegisterPage
