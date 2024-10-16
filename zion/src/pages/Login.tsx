a58387aa09e27fc9ef2e219c39f7d62de3a6b513
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-10-16 18:38:25 -0400

    Login page begun

--- zion/src/pages/Login.tsx
+++ zion/src/pages/Login.tsx
@@ -4,18 +4,28 @@ function LoginPage() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+      <h3 className="text-black text-3xl mb-8">Log in</h3>
+      <form action="">
+        <div className="mb-4 flex flex-col items-start">
+          <label htmlFor="email">Email</label>
+          <input className="border-2 border-gray-300" id="email "type="text" />
+          <div></div>
+        </div>
+        <div className="mb-4 flex flex-col items-start">
+          <label htmlFor="password">Password</label>
+          <input className="border-2 border-gray-300" id="password" type="password" />
+          <div></div>
+        </div>
+        <div>
+          <button>Log in</button>
+        </div>
+      </form>
+    </div>
+
+  )
+
 }
 
 export default LoginPage
-// kilimanjaro mountain .comnm site scout jgfdfhuige
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-
-    //   ododscbked
-    //   dfkas'd9-cxx  
-
-    //   dicjx  
-    //   l---=-=<u><object data="1
-    //   @EWPed 
-      
-    //   \]0" type=""></object></u>
-    // ]
