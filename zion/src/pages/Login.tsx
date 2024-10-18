499024a85b58231618dbe62decc84962b2837ae0
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-10-18 14:33:12 -0400

    Settled fonts out

--- zion/src/pages/Login.tsx
+++ zion/src/pages/Login.tsx
@@ -1,65 +1,65 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import Header from "../components/Header";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
 
 function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   // const navigate = useNavigate();
 
   return (
-    <div className="w-full lg:flex flex-col items-center">
+    <div className='w-full lg:flex flex-col items-center'>
       <Header />
-      <div className="w-full flex flex-col justify-center items-center lg:max-w-md lg:w-1/3 lg:border lg:border-gray-300 lg:bg-gray-50">
-        <h3 className="text-black text-3xl mb-8 md:mt-8">Log in</h3>
-        <form className="w-5/6 md:w-5/12 lg:w-5/6" action="">
-          <div className="mb-8 flex flex-col items-start">
-            <label className="text-black" htmlFor="email">
+      <div className='w-full flex flex-col justify-center items-center lg:max-w-md lg:w-5/12 lg:border lg:border-gray-300 lg:bg-gray-50'>
+        <h3 className='text-black text-3xl mb-8 md:mt-8'>Log in</h3>
+        <form className='w-5/6 md:w-5/12 lg:w-5/6' action=''>
+          <div className='mb-8 flex flex-col items-start'>
+            <label className='text-gray-500 text-sm' htmlFor='email'>
               Email
             </label>
             <input
-              className="w-full h-9 pl-2 pr-6 border border-gray-300 focus:outline-none focus:border-black text-black rounded"
-              id="email"
-              type="email"
+              className='transition duration-150 ease-linear w-full h-9 pl-2 pr-6 border border-gray-300 focus:outline-none focus:border-black text-black rounded hover:border-gray-400'
+              id='email'
+              type='email'
             />
           </div>
-          <div className="mb-8 flex flex-col items-start">
-            <label className="text-black" htmlFor="password">
+          <div className='mb-8 flex flex-col items-start'>
+            <label className='text-gray-500 text-sm' htmlFor='password'>
               Password
             </label>
             <input
-              className="w-full h-9 pl-2 pr-6 border border-gray-300 focus:outline-none focus:border-black text-black rounded"
-              id="password"
-              type="password"
+              className='transition duration-150 ease-linear w-full h-9 pl-2 pr-6 border border-gray-300 focus:outline-none focus:border-black text-black rounded hover:border-gray-400'
+              id='password'
+              type='password'
             />
           </div>
-          <div className="w-full flex justify-between mb-8">
+          <div className='w-full flex justify-between mb-8'>
             <div>
               <input
-                className="mr-2"
-                type="checkbox"
-                name="rememberme"
-                id="rememberme"
+                className='mr-2'
+                type='checkbox'
+                name='rememberme'
+                id='rememberme'
               />
-              <label className="text-black" htmlFor="rememberme">
+              <label className='text-black' htmlFor='rememberme'>
                 Remember Me
               </label>
             </div>
-            <a href="http://">Forgot Password</a>
+            <a href='http://'>Forgot Password</a>
           </div>
           <div>
-            <button className="w-full">Log in</button>
+            <button className='w-full'>Log in</button>
           </div>
-          <hr className="border-t-2 border-black w-full h-2 mt-8" />
+          <hr className='border-t-2 border-black w-full h-2 mt-8' />
         </form>
-        <div className="w-5/6 flex justify-center text-black mt-8 lg:mb-8">
+        <div className='w-5/6 flex justify-center text-black mt-8 lg:mb-8'>
           Don't have an account?
-          <Link className="ml-2" to="/register">
+          <Link className='ml-2' to='/register'>
             Sign up today
           </Link>
         </div>
       </div>
-      <footer className="w-full fixed bottom-0 left-0 text-center mt-8 mb-8 text-gray-300">
+      <footer className='w-full fixed bottom-0 left-0 text-center mt-8 mb-8 text-gray-300'>
         © Scout 2024
       </footer>
     </div>
--- zion/tailwind.config.js
+++ zion/tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'Arial', 'sans-serif'],
+      serif: ['"IM Fell DW Pica"', 'Georgia', 'serif'],
+    },
   },
   plugins: [],
-}
-
+};
