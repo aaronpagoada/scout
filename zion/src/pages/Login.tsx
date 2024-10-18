ad67a3e8f5d0f206624defed59b7bd4287fce637
AP <74792335+aaronpagoada@users.noreply.github.com>
2024-10-18 01:41:46 -0400

    Login style on breakpoints

--- zion/src/pages/Login.tsx
+++ zion/src/pages/Login.tsx
@@ -6,11 +6,11 @@ function LoginPage() {
   const [password, setPassword] = useState("");
 
   return (
-    <>
+    <div className="w-full lg:flex flex-col items-center">
       <Header />
-      <div className="w-full flex flex-col justify-center items-center">
-        <h3 className="text-black text-3xl mb-8">Log in</h3>
-        <form className="w-5/6" action="">
+      <div className="w-full flex flex-col justify-center items-center lg:max-w-md lg:w-1/3 lg:border lg:border-gray-300 lg:bg-gray-50">
+        <h3 className="text-black text-3xl mb-8 md:mt-8">Log in</h3>
+        <form className="w-5/6 md:w-5/12 lg:w-5/6" action="">
           <div className="mb-8 flex flex-col items-start">
             <label className="text-black" htmlFor="email">
               Email
@@ -48,9 +48,9 @@ function LoginPage() {
           <div>
             <button className="w-full">Log in</button>
           </div>
+          <hr className="border-t-2 border-black w-full h-2 mt-8" />
         </form>
-        <hr className="border-t-2 border-black w-5/6 h-2 mt-8" />
-        <div className="w-5/6 flex justify-center text-black mt-7">
+        <div className="w-5/6 flex justify-center text-black mt-8 lg:mb-8">
           Don't have an account?
           <a className="ml-2" href="http://localhost:5173/register">
             Sign up today
@@ -60,7 +60,7 @@ function LoginPage() {
       <footer className="w-full fixed bottom-0 left-0 text-center mt-8 mb-8 text-gray-300">
         © Scout 2024
       </footer>
-    </>
+    </div>
   );
 }
 
