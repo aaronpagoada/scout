c7530a8162581d0dc987714a9b2bce33604695d9
Aaron Pagoada <74792335+aaronpagoada@users.noreply.github.com>
2025-01-19 15:44:43 -0500

    Added dashboard

---
+++ zion/src/pages/DashboardPage.tsx
@@ -0,0 +1,11 @@
+function DashboardPage() {
+  const name = "Aaron"
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <div>{`Hi there ${name}`}</div>
+    </div>
+  )
+}
+
+export default DashboardPage
