04ed4a3d5abc3616b2d124a1a05b872fd5970c1e
Aaron Pagoada <74792335+aaronpagoada@users.noreply.github.com>
2025-01-19 15:18:22 -0500

    Landing buttons

--- zion/src/pages/LandingPage.tsx
+++ zion/src/pages/LandingPage.tsx
@@ -8,8 +8,14 @@ function LandingPage() {
 
   return (
     <div className='min-h-screen bg-gray-100 flex flex-col'>
-      {/* Header */}
       <Header />
+      <div className='text-black'>
+        <h2>Getting outside begins with <strong>Scout</strong></h2>
+        <div className='text-white'>
+          <Link to="/register"><button>Register</button></Link>
+          <Link to="/login"><button>Login</button></Link>
+        </div>
+      </div>
     </div>
   );
 }
